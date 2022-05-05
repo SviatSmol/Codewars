a4e322ff478fef1487169d62cf700a90df6f712a
SviatSmol <37250837+SviatSmol@users.noreply.github.com>
2022-05-05 21:07:12 +0300

    Create Write Number in Expanded Form.swift

---
+++ Write Number in Expanded Form.swift
@@ -0,0 +1,15 @@
+func expandedForm(_ num: Int) -> String {
+    
+    var intArray = String(num).map { Int(String($0)) ?? 0 }
+    
+    let countOfZero = intArray.count - 1
+    
+    var mappedArray: [Int] = []
+    
+    for i in 0...countOfZero {
+        mappedArray.append(intArray.remove(at: countOfZero - i))
+        intArray = intArray.map{ $0 * 10 }
+    }
+    
+  return mappedArray.filter{ $0 > 0 }.reversed().map { String($0) }.joined(separator: " + ")
+}
